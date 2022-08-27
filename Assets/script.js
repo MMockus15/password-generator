8290d0fbd32238d760fd178ffd44a1b3d4947ae2
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-08-27 10:48:39 -0600

    added psuedo code

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+upperCase = upperCase.concat(upperCase2);
+console.log(upperCase);
+// upperCase = upperCase.push("F", "G");
+console.log(upperCase.length);
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +14,14 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  // write code to generate the users password
+  // prompt the user - length (8-128)
+  // confirm use of - uppercase, lowercase, numbers, specChars
+  // Generate a user password that meets the selected criteria
+  // make sure that all of the users character selections are included in the output
 }
 
 // Add event listener to generate button
